23744e7d194c00b897288d517927012c4e1fab2a
Vicente Soler <91003414+vsolgar@users.noreply.github.com>
2022-02-20 12:23:01 +0100

    Update Main.java
    Cambio hecho a mano desde la página web

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+/* Cambio introducido a mano en la rama refactorizar
+    para ver como funciona de bien un PULL
+*/
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo del Entorno");
         System.out.println("Primera refactorizacion.");
+        System.out.println("Si se muestra esto, es que el PULL ha funcionado");
     }
 }
